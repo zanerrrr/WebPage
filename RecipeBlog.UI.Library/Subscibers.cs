e12ab2c83cb8fe7be85ef85bd0453c25e5498af8
zanerrrr <zanerubene1995@gmail.com>
2021-12-13 11:40:16 +0200

    +++

---
+++ RecipeBlog.UI.Library/Subscibers.cs
@@ -0,0 +1,11 @@
+﻿namespace RecipeBlogSubscribers.UI.Library
+{
+    public class Subscribers
+    {
+        public static List<Subscribers> ListOfSubscribers { get; set; }
+
+    }
+
+    
+    
+}
