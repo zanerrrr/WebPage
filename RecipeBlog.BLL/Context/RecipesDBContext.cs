574aa0fbfddf51edf57702d777f2e58a73915fac
zanerrrr <zanerubene1995@gmail.com>
2021-12-15 23:31:03 +0200

    =

---
+++ RecipeBlog.BLL/Context/RecipesDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RecipeBlog.BLL.Entities;
+
+namespace RecipeBlog.BLL.Context
+{
+	public class RecipesDBContext : DbContext
+	{
+		public DbSet<Recipes> Recipes { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+			optionsBuilder.UseSqlServer(@"Server=DESKTOP-S62MUI2\SQLEXPRESS; Database=SubscribersDB; Trusted_Connection = true;");
+	}
+}
