8702dc0325683d5aa8d17200fd4a0093f7257e2a
zanerrrr <zanerubene1995@gmail.com>
2021-12-16 14:55:39 +0200

    +

---
+++ FinalProject/Models/RecipesModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RecipeBlog.UI.Models
+{
+    public class RecipesModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Ingredients { get; set; }
+        public string Description { get; set; }
+    }
+}
--- RecipeBlog.BLL/Entities/Recipes.cs
+++ RecipeBlog.BLL/Entities/Recipes.cs
@@ -12,6 +12,7 @@ namespace RecipeBlog.BLL.Entities
 		[Key]
 		public int Id { get; set; }
 		public string Title { get; set; }
+		public string Ingredients { get; set; }
 		public string Description { get; set; }
 
 	}
